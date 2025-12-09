a294cc89590717eb32e6fca31c50eb66e3690c78
Tariq Ameer <1tariqameer@gmail.com>
2025-12-09 16:50:05 +0000

    Update config

--- keyboards/keychron/q2/iso_encoder/keymaps/tqja/keymap.c
+++ keyboards/keychron/q2/iso_encoder/keymaps/tqja/keymap.c
@@ -59,8 +59,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FN3] = LAYOUT_iso_68(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_DEL,          _______,
-        _______, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  _______, _______, _______, _______, _______, _______,  _______,                    _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
+        _______, KC_F13,  KC_UP,   KC_F14,  KC_F15,  KC_F16,  KC_F17, _______, _______, _______, _______, _______,  _______,                    _______,
+        _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______,          _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, KC_MUTE,
         _______, _______, _______,                            _______,                            _______, _______,  _______,  KC_MPRV, KC_MPLY, KC_MNXT)
 };
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [MAC_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [WIN_BASE] = { ENCODER_CCW_CW(KC_F23,  KC_F24)  },
     [_FN1]     = { ENCODER_CCW_CW(_______, _______) },
     [_FN2]     = { ENCODER_CCW_CW(_______, _______) },
-    [_FN3]     = { ENCODER_CCW_CW(_______, _______) },
+    [_FN3]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
 };
 #endif
